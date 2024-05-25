00a84525eb910b9dd3287578d6c61cc835a12e75
Matija Medvidovic <matija.medvidovic@gmail.com>
2024-05-25 18:01:52 -0400

    Added hydrogen atom wavefunctions as basis

--- ggda/pool.py
+++ ggda/pool.py
@@ -151,3 +151,123 @@ class SphericalVectorPool(nn.Module):
             ],
             dim=-1,
         )
+
+
+class HydrogenPool(nn.Module):
+    def __init__(self, n_max: float, max_scale: float):
+
+        super().__init__()
+
+        self.n_max = n_max
+        self.n_basis = (n_max * (1 + n_max) * (1 + 2 * n_max)) // 6
+        self.max_scale = max_scale
+        self.bohr = max_scale / n_max
+
+        # fmt: off
+        norms_1 = torch.tensor([1])
+        norms_2 = 32 * torch.tensor([1, 1, 1, 1])
+        norms_3 = 81**2 * torch.tensor([3, 2, 2, 2, 2, 2, 6, 2, 2])
+        norms_4 = 512**2 * torch.tensor([9, 5, 5, 5, 3, 3, 36, 3, 12, 72, 3, 120, 180, 120, 12, 72])
+        # fmt: on
+
+        self.register_buffer("scales_1", torch.rsqrt(torch.pi * self.bohr**3 * norms_1))
+        self.register_buffer("scales_2", torch.rsqrt(torch.pi * self.bohr**3 * norms_2))
+        self.register_buffer("scales_3", torch.rsqrt(torch.pi * self.bohr**3 * norms_3))
+        self.register_buffer("scales_4", torch.rsqrt(torch.pi * self.bohr**3 * norms_4))
+
+    def _eval_1(self, d: LazyTensor, F: LazyTensor, reduce_dim: int) -> LazyTensor:
+        r = d.norm2()
+        kernel = (-r).exp()
+        return (kernel * F).sum_reduction(dim=reduce_dim) * self.scales_1[0]
+
+    def _eval_2(self, d: LazyTensor, F: LazyTensor, reduce_dim: int) -> LazyTensor:
+
+        x, y, z, r = d[0], d[1], d[2], d.norm2()
+        kernel = (-r / 2).exp()
+
+        fconv = [
+            (-kernel * (2 - r) * F).sum_reduction(dim=reduce_dim),
+            -(kernel * x * F).sum_reduction(dim=reduce_dim),
+            (kernel * z * F).sum_reduction(dim=reduce_dim),
+            -(kernel * y * F).sum_reduction(dim=reduce_dim),
+        ]
+
+        return torch.cat(fconv, dim=-1) * self.scales_2
+
+    def _eval_3(self, d: LazyTensor, F: LazyTensor, reduce_dim: int) -> LazyTensor:
+
+        x, y, z, r = d[0], d[1], d[2], d.norm2()
+        kernel = (-r / 3).exp()
+
+        fconv = [
+            (kernel * (27 + 2 * (-9 + r) * r) * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-6 + r) * x * F).sum_reduction(dim=reduce_dim),
+            -(kernel * (-6 + r) * z * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-6 + r) * y * F).sum_reduction(dim=reduce_dim),
+            (kernel * x * y * F).sum_reduction(dim=reduce_dim),
+            -(kernel * x * z * F).sum_reduction(dim=reduce_dim),
+            (kernel * (2 * r**2 - 3 * (x**2 + y**2)) * F).sum_reduction(dim=reduce_dim),
+            -(kernel * y * z * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-(x**2) + y**2) * F).sum_reduction(dim=reduce_dim),
+        ]
+
+        return torch.cat(fconv, dim=-1) * self.scales_3
+
+    def _eval_4(self, d: LazyTensor, F: LazyTensor, reduce_dim: int) -> LazyTensor:
+
+        x, y, z, r = d[0], d[1], d[2], d.norm2()
+        kernel = (-r / 4).exp()
+
+        # fmt: off
+        fconv = [
+            (kernel * (192 - (-12 + r) ** 2 * r) * F).sum_reduction(dim=reduce_dim),
+            -(kernel * (80 + (-20 + r) * r) * x * F).sum_reduction(dim=reduce_dim),
+            (kernel * (80 + (-20 + r) * r) * z * F).sum_reduction(dim=reduce_dim),
+            -(kernel * (80 + (-20 + r) * r) * y * F).sum_reduction(dim=reduce_dim),
+            -(kernel * (-12 + r) * x * y * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-12 + r) * x * z * F).sum_reduction(dim=reduce_dim),
+            -(kernel * (-12 + r) * (2 * r**2 - 3 * (x**2 + y**2)) * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-12 + r) * y * z * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-12 + r) * (x - y) * (x + y) * F).sum_reduction(dim=reduce_dim),
+            (kernel * x * (x**2 - 3 * y**2) * F).sum_reduction(dim=reduce_dim),
+            (kernel * x * y * z * F).sum_reduction(dim=reduce_dim),
+            (kernel * x * (-4 * r**2 + 5 * (x**2 + y**2)) * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-3 * (x**2 + y**2) * z + 2 * z**3) * F).sum_reduction(dim=reduce_dim),
+            (kernel * y * (-4 * r**2 + 5 * (x**2 + y**2)) * F).sum_reduction(dim=reduce_dim),
+            (kernel * (-(x**2) + y**2) * z * F).sum_reduction(dim=reduce_dim),
+            -(kernel * y * (-3 * x**2 + y**2) * F).sum_reduction(dim=reduce_dim),
+        ]
+        # fmt: on
+
+        return torch.cat(fconv, dim=-1) * self.scales_4
+
+    def forward(self, f: Tensor, coords: Tensor, out_coords: Optional[Tensor] = None) -> Tensor:
+
+        if out_coords is None:
+            out_coords = coords
+
+        reduce_dim = (f.ndim - 1) + 1
+
+        # fmt: off
+        F = LazyTensor(rearrange(                     f, "... i   -> ... 1 i 1").contiguous())
+        r = LazyTensor(rearrange(    coords / self.bohr, "... i c -> ... 1 i c").contiguous())
+        R = LazyTensor(rearrange(out_coords / self.bohr, "... a c -> ... a 1 c").contiguous())
+        # fmt: on
+
+        d = R - r
+
+        fconv = [self._eval_1(d, F, reduce_dim)]
+
+        if self.n_max >= 2:
+            fconv.append(self._eval_2(d, F, reduce_dim))
+
+        if self.n_max >= 3:
+            fconv.append(self._eval_3(d, F, reduce_dim))
+
+        if self.n_max >= 4:
+            fconv.append(self._eval_4(d, F, reduce_dim))
+
+        if self.n_max >= 5:
+            raise NotImplementedError("n_max >= 5 is not implemented.")
+
+        return torch.cat(fconv, dim=-1)
