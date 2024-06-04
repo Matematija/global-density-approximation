b301393d53fb53f22a552ca0f7697e232080eb80
Matija Medvidovic <mmedvidovic-visitor@flatironinstitute.org>
2024-06-03 22:13:42 -0400

    Added both LDA X and C

--- ggda/gda.py
+++ ggda/gda.py
@@ -2,14 +2,15 @@ from typing import Optional
 
 import torch
 from torch import nn
+from torch.nn import functional as F
 from torch import Tensor
 
 from .pool import CoarseGraining
 from .encoder import Encoder
 from .decoder import Decoder
 from .layers import MLP
-from .features import lda, mean_and_covariance
-from .utils import Activation, cubic_grid
+from .features import lda_x, lda_c, mean_and_covariance
+from .utils import Activation, cubic_grid, log_cosh
 
 
 class GlobalDensityApprox(nn.Module):
@@ -45,7 +46,7 @@ class GlobalDensityApprox(nn.Module):
 
         self.decoder = Decoder(
             embed_dim=embed_dim,
-            out_features=2,
+            out_features=3,
             n_blocks=n_decoder_blocks,
             n_heads=n_heads,
             coord_std=coord_std,
@@ -53,14 +54,14 @@ class GlobalDensityApprox(nn.Module):
             activation=activation,
         )
 
-        self.nonlocal_head = MLP(embed_dim, enhancement, activation, out_features=1)
+        # self.nonlocal_head = MLP(embed_dim, enhancement, activation, out_features=1)
 
     def setup_grid(self, wrho: Tensor, coords: Tensor) -> Tensor:
 
         means, covs = mean_and_covariance(wrho, coords)
         s2, R = torch.linalg.eigh(covs)
 
-        coords = (coords - means.unsqueeze(-2)) @ R.mT
+        coords = (coords - means.unsqueeze(-2)) @ R.mT.detach()
         anchor_coords = 3 * torch.sqrt(s2 + 1e-5).unsqueeze(-2) * self.grid
 
         return coords, anchor_coords
@@ -73,11 +74,14 @@ class GlobalDensityApprox(nn.Module):
         phi = self.pooling(rho, gamma, coords, weights, anchor_coords)
         context = self.encoder(phi, anchor_coords)
 
-        scale, bias = self.decoder(rho, gamma, coords, context).unbind(dim=-1)
+        scale_x, scale_c, bias = self.decoder(rho, gamma, coords, context).unbind(dim=-1)
+        scale_x, scale_c, bias = F.softplus(scale_x), F.softplus(scale_c), -log_cosh(bias)
 
-        lda_val = lda(rho.clip(min=1e-7))
+        rho_ = rho.clip(min=1e-5)
+        lda_x_val, lda_c_val = lda_x(rho_), lda_c(rho_)
+        exc = scale_x * lda_x_val + scale_c * lda_c_val + bias
 
-        E_local = torch.sum(wrho * (scale * lda_val + bias), dim=-1)
-        E_nonlocal = self.nonlocal_head(context).squeeze(-1).mean(-1)
+        E_local = torch.sum(wrho * exc, dim=-1)
+        # E_nonlocal = -log_cosh(self.nonlocal_head(context)).squeeze(-1).mean(-1)
 
-        return E_local + E_nonlocal
+        return E_local  # + E_nonlocal
