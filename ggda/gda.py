a994d140cdf39928df68c202f93deddd103a3cff
Matija Medvidovic <mmedvidovic-visitor@flatironinstitute.org>
2024-06-24 17:10:29 -0400

    "Beta" version

--- ggda/gda.py
+++ ggda/gda.py
@@ -1,27 +1,33 @@
-from typing import Optional
-
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch import Tensor
 
-from .layers import MLP, LinearSelfAttention
-from .features import lda_x, lda_c, mean_and_covariance
-from .utils import Activation, activation_func, log_cosh
+from .embed import CiderFeatures
+from .layers import GatedMLP, FourierAttention
+from .features import rescaled_grad, mean_and_covariance
+from .utils import Activation, activation_func
 
 
 class DensityEmbedding(nn.Module):
-    def __init__(self, embed_dim: int, enhancement: float = 4.0, eps: float = 1e-4):
+    def __init__(
+        self, embed_dim: int, embed_params: tuple[float, float] = (4.0, 4.0), eps: float = 1e-4
+    ):
 
         super().__init__()
-
         self.eps = eps
 
-        width = int(enhancement * embed_dim)
-        self.lift = nn.Sequential(nn.Linear(2, width), nn.Tanh(), nn.Linear(width, embed_dim))
+        self.nonlocal_features = CiderFeatures(*embed_params)
+        self.lift = nn.Linear(4, embed_dim)
 
-    def forward(self, rho: Tensor, gamma: Tensor) -> Tensor:
-        x = torch.stack([rho, gamma], dim=-1)
+    def forward(
+        self, rho: Tensor, gamma: Tensor, coords: Tensor, weights: Tensor, *args, **kwargs
+    ) -> Tensor:
+
+        x0 = rescaled_grad(rho + 1e-12, gamma).unsqueeze(-1)
+        x1 = self.nonlocal_features(rho, gamma, coords, weights, *args, **kwargs)
+        x = torch.cat([x0, x1], dim=-1)
+
         return self.lift(torch.log(x + self.eps))
 
 
@@ -29,47 +35,39 @@ class Block(nn.Module):
     def __init__(
         self,
         embed_dim: int,
-        min_scale: float,
-        n_heads: Optional[int] = None,
+        kernel_scale: float,
         enhancement: float = 4.0,
         activation: Activation = "silu",
     ):
 
         super().__init__()
 
-        self.attention = LinearSelfAttention(embed_dim, min_scale, n_heads)
-        self.mlp = MLP(embed_dim, enhancement, activation=activation)
+        self.attention = FourierAttention(embed_dim, kernel_scale)
+        self.mlp = GatedMLP(embed_dim, enhancement, activation=activation)
 
-        self.attn_norm = nn.LayerNorm(embed_dim)
-        self.mlp_norm = nn.LayerNorm(embed_dim)
-
-    def forward(
-        self, phi: Tensor, rho: Tensor, gamma: Tensor, coords: Tensor, weights: Tensor
-    ) -> Tensor:
-
-        attn = self.attention(phi, rho, gamma, coords, weights)
-        phi = self.attn_norm(phi + attn)
-
-        return self.mlp_norm(phi + self.mlp(phi))
+    def forward(self, phi: Tensor, coords: Tensor, weights: Tensor) -> Tensor:
+        phi = phi + self.attention(phi, coords, weights)
+        return phi + self.mlp(F.tanh(phi))
 
 
 class FieldProjection(nn.Module):
     def __init__(
         self,
         embed_dim: int,
-        out_features: int,
-        enhancement: float = 4.0,
+        out_features: int = 1,
+        enhancement: float = 2.0,
         activation: Activation = "silu",
     ):
 
         super().__init__()
 
         self.activation = activation_func(activation)
-        self.mlp = MLP(embed_dim, enhancement, activation)
+        self.mlp = GatedMLP(embed_dim, enhancement, activation)
         self.proj = nn.Linear(embed_dim, out_features)
 
     def __call__(self, phi: Tensor) -> Tensor:
-        return self.proj(self.activation(self.mlp(phi)))
+        phi = self.mlp(F.tanh(phi))
+        return self.proj(self.activation(phi))
 
 
 class GlobalDensityApprox(nn.Module):
@@ -77,20 +75,20 @@ class GlobalDensityApprox(nn.Module):
         self,
         embed_dim: int,
         n_blocks: int,
-        min_scale: float,
-        n_heads: int = None,
-        enhancement: float = 4.0,
+        kernel_scale: float,
+        embed_params: tuple[float, float] = (4.0, 4.0),
+        enhancement: float = 2.0,
         activation: Activation = "silu",
     ):
 
         super().__init__()
 
-        self.embedding = DensityEmbedding(embed_dim, enhancement)
+        self.embedding = DensityEmbedding(embed_dim, embed_params)
 
-        make_block = lambda: Block(embed_dim, min_scale, n_heads, enhancement, activation)
+        make_block = lambda: Block(embed_dim, kernel_scale, enhancement, activation)
         self.blocks = nn.ModuleList([make_block() for _ in range(n_blocks)])
 
-        self.proj = FieldProjection(embed_dim, 3, enhancement, activation)
+        self.proj = FieldProjection(embed_dim, 1, enhancement, activation)
 
     def rotate_coords(self, wrho: Tensor, coords: Tensor) -> Tensor:
         means, covs = mean_and_covariance(wrho, coords)
@@ -100,12 +98,9 @@ class GlobalDensityApprox(nn.Module):
     def forward(self, rho: Tensor, gamma: Tensor, coords: Tensor, weights: Tensor) -> Tensor:
 
         coords = self.rotate_coords(weights * rho, coords)
-        phi = self.embedding(rho, gamma)
+        phi = self.embedding(rho, gamma, coords, weights)
 
         for block in self.blocks:
-            phi = block(phi, rho, gamma, coords, weights)
+            phi = block(phi, coords, weights)
 
-        scale_x, scale_c, bias = self.proj(phi).unbind(dim=-1)
-        scale_x, scale_c, bias = F.softplus(scale_x), F.softplus(scale_c), -log_cosh(bias)
-
-        return scale_x * lda_x(rho + 1e-7) + scale_c * lda_c(rho + 1e-7) + bias
+        return self.proj(phi).squeeze(dim=-1)
--- ggda/layers.py
+++ ggda/layers.py
@@ -1,11 +1,11 @@
 from typing import Optional
+from math import sqrt
 
 import torch
 from torch import nn
-from torch import Tensor
+from torch import Tensor, BoolTensor
 
-from .features import rescaled_grad
-from .utils import Activation, activation_func, random_unit_vec, log_cosh
+from .utils import Activation, activation_func
 
 
 class MLP(nn.Module):
@@ -14,7 +14,6 @@ class MLP(nn.Module):
         features: int,
         enhancement: float,
         activation: Activation = "gelu",
-        n_hidden: int = 0,
         out_features: Optional[int] = None,
         bias: bool = True,
     ):
@@ -28,130 +27,244 @@ class MLP(nn.Module):
         width = int(features * enhancement)
 
         self.in_linear = nn.Linear(in_features, width, bias=bias)
-        self.hidden = nn.ModuleList([nn.Linear(width, width, bias=bias) for _ in range(n_hidden)])
         self.out_linear = nn.Linear(width, out_features, bias=bias)
 
     def forward(self, x: Tensor) -> Tensor:
-
-        h = self.activation(self.in_linear(x))
-
-        for linear in self.hidden:
-            h = self.activation(linear(h))
-
-        return self.out_linear(h)
+        return self.out_linear(self.activation(self.in_linear(x)))
 
 
-class CoordinateEncoding(nn.Module):
-    def __init__(self, n_modes: int, init_std: float, ndim: int = 3):
-
-        super().__init__()
-
-        assert n_modes % 2 == 0, "Number of modes must be even."
-        self.n_modes = n_modes
-
-        self.mode_lift = nn.Linear(ndim, n_modes // 2, bias=False)
-        nn.init.normal_(self.mode_lift.weight, std=1 / init_std)
-
-    def forward(self, coords: Tensor) -> Tensor:
-        emb = self.mode_lift(coords)
-        return torch.cat([torch.cos(emb), torch.sin(emb)], dim=-1)
-
-
-def basis_norm(psi: Tensor, weights: Optional[Tensor], eps: float = 1e-5) -> Tensor:
-
-    psi2 = torch.abs(psi) ** 2
-
-    if weights is None:
-        norm_squared = torch.sum(psi2, dim=-2, keepdim=True)
-
-    elif torch.is_floating_point(weights):
-        w = weights.unsqueeze(-1)
-        norm_squared = torch.sum(w * psi2, dim=-2, keepdim=True)
-
-    else:
-        psi2 = psi2.masked_fill(~weights.unsqueeze(-1), 0.0)
-        norm_squared = torch.sum(psi2, dim=-2, keepdim=True)
-
-    return psi / torch.sqrt(norm_squared + eps)
-
-
-class RotaryPositionalEncoding(nn.Module):
-    def __init__(self, embed_dim: int, min_scale: float, base: float = 100.0, ndim: int = 3):
-
-        super().__init__()
-
-        assert embed_dim % 2 == 0, "Embedding dimension must be even."
-        self.lift = nn.Linear(ndim, embed_dim // 2, bias=False)
-
-        with torch.no_grad():
-            scales = base ** (-2 * torch.arange(0, embed_dim // 2) / embed_dim)
-            k_norms = (2 * torch.pi / min_scale) * scales
-            k_vals = k_norms.unsqueeze(-1) * random_unit_vec(embed_dim // 2)
-            self.lift.weight.copy_(k_vals)
-
-    def forward(self, f: Tensor, coords: Tensor) -> Tensor:
-
-        phase = self.lift(coords)  # (..., n_grid, embed_dim // 2)
-        c, s = torch.cos(phase), torch.sin(phase)
-        f1, f2 = torch.chunk(f, 2, dim=-1)
-
-        return torch.cat([f1 * c - f2 * s, f1 * s + f2 * c], dim=-1)
-
-
-class LinearSelfAttention(nn.Module):
+class GatedMLP(nn.Module):
     def __init__(
-        self, embed_dim: int, min_scale: float, n_heads: Optional[int] = None, bias: bool = True
+        self,
+        features: int,
+        enhancement: float,
+        activation: Activation = "gelu",
+        out_features: Optional[int] = None,
+        bias: bool = True,
     ):
 
         super().__init__()
 
-        self.embed_dim = embed_dim
-        self.n_heads = n_heads or max(embed_dim // 64, 1)
-        self.head_dim = embed_dim // self.n_heads
-        self.inner_dim = self.head_dim * self.n_heads
+        self.activation = activation_func(activation)
 
-        self.query_proj = nn.Linear(embed_dim, self.inner_dim, bias=bias)
-        self.key_proj = nn.Linear(embed_dim, self.inner_dim, bias=bias)
-        self.value_proj = nn.Linear(embed_dim, self.inner_dim, bias=bias)
-        self.out_proj = nn.Linear(self.inner_dim, embed_dim, bias=bias)
+        in_features = features
+        out_features = out_features or features
+        width = int(features * enhancement)
 
-        self.key_density_weight = nn.Linear(1, self.inner_dim, bias=bias)
-        self.value_density_weight = nn.Linear(1, self.inner_dim, bias=bias)
+        self.in_linear = nn.Linear(in_features, 2 * width, bias=bias)
+        self.out_linear = nn.Linear(width, out_features, bias=bias)
 
-        self.pos_enc = RotaryPositionalEncoding(embed_dim, min_scale)
+    def forward(self, x: Tensor) -> Tensor:
+        a, b = self.in_linear(x).chunk(2, dim=-1)
+        return self.out_linear(a * self.activation(b))
 
-        self._initialize_query_proj()
 
-    def _initialize_query_proj(self):
+# def basis_norm(psi: Tensor, weights: Optional[Tensor], eps: float = 1e-5) -> Tensor:
 
-        delta, sigma = 1 / self.head_dim, 1 / self.head_dim
+#     psi2 = torch.abs(psi) ** 2
+
+#     if weights is None:
+#         norm_squared = torch.sum(psi2, dim=-2, keepdim=True)
+
+#     elif torch.is_floating_point(weights):
+#         w = weights.unsqueeze(-1)
+#         norm_squared = torch.sum(w * psi2, dim=-2, keepdim=True)
+
+#     else:
+#         psi2 = psi2.masked_fill(~weights.unsqueeze(-1), 0.0)
+#         norm_squared = torch.sum(psi2, dim=-2, keepdim=True)
+
+#     return psi / torch.sqrt(norm_squared + eps)
+
+
+def orbital_norm(psi: Tensor, weights: Tensor, eps: float = 1e-5) -> Tensor:
+    norm_squared = torch.einsum("...x,...xi->...i", weights, torch.abs(psi) ** 2)
+    return psi / torch.sqrt(norm_squared + eps).unsqueeze(-2)
+
+
+# def gaussian_kernel(x: Tensor, y: Tensor, sigma: float) -> Tensor:
+#     norm = (2 * torch.pi * sigma**2) ** (x.shape[-1] / 2)
+#     d2 = torch.sum((x[..., :, None, :] - y[..., None, :, :]) ** 2, dim=-1)
+#     return torch.exp(-d2 / (2 * sigma**2)) / norm
+
+
+# def interpolated_gaussian_kernel(
+#     x: Tensor,
+#     sigma: float,
+#     rank: int,
+#     weights: Optional[Tensor] = None,
+#     generator: Optional[Generator] = None,
+#     **lstsq_kwargs,
+# ) -> tuple[Tensor, Tensor]:
+
+#     if weights is None:
+#         weights = torch.ones_like(x[..., 0])
+
+#     idx = torch.multinomial(weights, rank, replacement=False, generator=generator)
+#     y = torch.take_along_dim(x, idx.unsqueeze(-1), dim=-2)
+
+#     left = gaussian_kernel(y, x, sigma)
+#     middle = gaussian_kernel(y, y, sigma)
+#     right, *_ = torch.linalg.lstsq(middle, left, **lstsq_kwargs)
+
+#     return left, right
+
+
+# class FourierPositionalEncoding(nn.Module):
+#     def __init__(self, n_modes: int, kernel_scale: float, ndim: int = 3):
+
+#         super().__init__()
+
+#         self.n_modes = n_modes
+
+#         self.mode_lift = nn.Linear(ndim, n_modes)
+#         nn.init.normal_(self.mode_lift.weight, std=1 / kernel_scale)
+#         nn.init.uniform_(self.mode_lift.bias, a=-torch.pi, b=torch.pi)
+
+#     def forward(self, coords: Tensor) -> Tensor:
+#         return sqrt(2 / self.n_modes) * torch.cos(self.mode_lift(coords)).mT
+
+
+class RotaryPositionalEncoding(nn.Module):
+    def __init__(self, embed_dim: int, kernel_scale: float, ndim: int = 3):
+
+        super().__init__()
+
+        assert embed_dim % 2 == 0, "Embedding dimension must be even"
+
+        self.mode_lift = nn.Linear(ndim, embed_dim // 2, bias=False)
+        nn.init.normal_(self.mode_lift.weight, std=1 / kernel_scale)
+
+    def forward(self, x: Tensor, coords: Tensor) -> Tensor:
+
+        phases = self.mode_lift(coords)
+        c, s = torch.cos(phases), torch.sin(phases)
+        a, b = x.chunk(2, dim=-1)
+
+        return torch.cat([a * c - b * s, a * s + b * c], dim=-1)
+
+
+class LinearSelfAttention(nn.Module):
+    def __init__(self, embed_dim: int, kernel_scale: float, bias: bool = True):
+
+        super().__init__()
+
+        self.query_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.key_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.value_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+
+        self.positional_encoding = RotaryPositionalEncoding(embed_dim, kernel_scale)
+
+    @staticmethod
+    def _initialize_proj(proj: nn.Linear):
+
+        embed_dim = proj.weight.shape[-1]
+        delta, sigma = 1 / embed_dim, 1 / embed_dim
 
         with torch.no_grad():
 
-            A, B = torch.empty(2, self.inner_dim, self.embed_dim)
-            nn.init.orthogonal_(A, gain=1)
+            A, B = torch.zeros(2, embed_dim, embed_dim)
+            nn.init.orthogonal_(A, gain=sigma)
             nn.init.eye_(B)
 
-            self.query_proj.weight.copy_(sigma * A + delta * B)
+            proj.weight.copy_(A + delta * B)
 
-            if self.query_proj.bias is not None:
-                nn.init.zeros_(self.query_proj.bias)
+            if proj.bias is not None:
+                nn.init.zeros_(proj.bias)
 
-    def forward(
-        self, phi: Tensor, rho: Tensor, gamma: Tensor, coords: Tensor, weights: Tensor
-    ) -> Tensor:
+    def forward(self, phi: Tensor, coords: Tensor, weights: Tensor) -> Tensor:
 
-        query = self.query_proj(phi)
-        key = basis_norm(self.key_proj(phi), weights)
-        value = basis_norm(self.value_proj(phi), weights)
+        # query, key = self.query_proj(phi), self.key_proj(phi)
+        # value = self.value_proj(torch.tanh(phi))
 
-        x = rescaled_grad(rho + 1e-12, gamma).unsqueeze(-1)
-        key = key * torch.exp(-0.5 * self.key_density_weight(x) ** 2)
-        value = value * torch.exp(-0.5 * self.value_density_weight(x) ** 2)
+        query, key, value = self.query_proj(phi), self.key_proj(phi), self.value_proj(phi)
+        query, key, value = self._normalize(query, key, value, weights)
 
-        key = key * (weights * rho).unsqueeze(-1)
+        query = self.positional_encoding(query, coords)
+        key = self.positional_encoding(key, coords)
 
-        query = self.pos_enc(query, coords)
-        key = self.pos_enc(key, coords)
+        return query @ (key.mT @ value)
 
-        return self.out_proj(query @ (key.mT @ value))
+
+class GalerkinAttention(LinearSelfAttention):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._initialize_proj(self.query_proj)
+
+    def _normalize(self, query, key, value, weights):
+
+        key = orbital_norm(key, weights)
+        value = orbital_norm(value, weights) * weights.unsqueeze(-1)
+
+        return query, key, value
+
+
+class FourierAttention(LinearSelfAttention):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._initialize_proj(self.value_proj)
+
+    def _normalize(self, query, key, value, weights):
+
+        query = orbital_norm(query, weights)
+        key = orbital_norm(key, weights)
+        value = value * weights.unsqueeze(-1)
+
+        return query, key, value
+
+
+# class LinearSelfAttention(nn.Module):
+#     def __init__(self, embed_dim: int, kernel_rank: int, kernel_scale: float, bias: bool = True):
+
+#         super().__init__()
+
+#         self.rank, self.scale = kernel_rank, kernel_scale
+
+#         self.query_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+#         self.key_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+#         self.value_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+
+#     @staticmethod
+#     def _initialize_proj(proj: nn.Linear):
+
+#         embed_dim = proj.weight.shape[-1]
+#         delta, sigma = 1 / embed_dim, 1 / embed_dim
+
+#         with torch.no_grad():
+
+#             A, B = torch.empty(2, embed_dim, embed_dim)
+#             nn.init.orthogonal_(A, gain=1)
+#             nn.init.eye_(B)
+
+#             proj.weight.copy_(sigma * A + delta * B)
+
+#             if proj.bias is not None:
+#                 nn.init.zeros_(proj.bias)
+
+#     def forward(
+#         self, phi: Tensor, coords: Tensor, weights: Tensor, mask: Optional[BoolTensor] = None
+#     ) -> Tensor:
+
+#         if mask is None:
+#             mask = weights > 0
+
+#         point_weights = (weights > 0).to(weights.dtype)
+
+#         query, key, value = self.query_proj(phi), self.key_proj(phi), self.value_proj(phi)
+#         query, key, value = self._normalize(query, key, value, weights)
+
+#         query_pe, key_pe = interpolated_gaussian_kernel(
+#             coords, self.scale, self.rank, weights=point_weights
+#         )
+
+#         mask = ~mask.unsqueeze(-1)
+#         query = query.masked_fill(mask, 0.0)
+#         key = key.masked_fill(mask, 0.0)
+#         value = value.masked_fill(mask, 0.0)
+
+#         signature = "...xj, ...mx, ...my, ...yj, ...yi -> ...xi"
+#         return torch.einsum(signature, query, query_pe, key_pe, key, value)
+
+#         # query, key = query_pe @ query, key_pe @ key
+#         # return query @ (key.mT @ value)
+#         # Maybe add activation here in Galerkin case? GeLU(K.T @ V)?
