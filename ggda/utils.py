7ba12b15c60d2498cd0dd873bf4bdbb848e71d88
Matija Medvidovic <matija.medvidovic@gmail.com>
2024-10-08 12:10:21 +0200

    Deleted old code

--- ggda/utils.py
+++ ggda/utils.py
@@ -7,7 +7,7 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 from torch import Tensor, BoolTensor
-from torch import device as Device, dtype as Dtype
+
 
 Activation = Union[str, Callable[[Tensor], Tensor]]
 
